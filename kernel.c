9eb61268690df892282bec3e524d86fbca52f894
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-01-30 15:14:00 -0500

    Part 2, some abstractions

--- kernel.c
+++ kernel.c
@@ -1,9 +1,15 @@
-int main(void) {
-	char *string = "Hello, World!\n";
-	while(*string) {
-		*(volatile char *)0x101f1000 = *string;
-		string++;
+#include "versatilepb.h"
+
+void bwputs(char *s) {
+	while(*s) {
+		while(*(UART0 + UARTFR) & UARTFR_TXFF);
+		*UART0 = *s;
+		s++;
 	}
+}
+
+int main(void) {
+	bwputs("Starting\n");
 
 	while(1); /* We can't exit, there's nowhere to go */
 	return 0;
